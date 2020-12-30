80afb005d00b3b9773958c28a46347ac97662361
Felipe Lima <flf.felipe@gmail.com>
2020-12-30 12:21:11 -0300

    Habilita CORS

--- src/main/java/br/com/catsanddogs/catsanddogs/application/controller/CatsAndDogsController.java
+++ src/main/java/br/com/catsanddogs/catsanddogs/application/controller/CatsAndDogsController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/cats-and-dogs")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CatsAndDogsController {
 
     @Autowired
